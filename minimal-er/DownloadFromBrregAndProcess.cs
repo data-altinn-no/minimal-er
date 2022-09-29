7119705662503f417d49af068a232293b53caab8
Bjørn Dybvik Langfors <bdl@digdir.no>
2022-09-29 10:23:51 +0200

    Initial version

---
+++ minimal-er/DownloadFromBrregAndProcess.cs
@@ -0,0 +1,127 @@
+using System.Diagnostics;
+using System.Net;
+using Azure.Storage.Blobs;
+using ICSharpCode.SharpZipLib.GZip;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace minimal_er
+{
+    public class DownloadFromBrregAndProcess
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger _logger;
+
+        public DownloadFromBrregAndProcess(ILoggerFactory loggerFactory, IConfiguration configuration, IHttpClientFactory clientFactory)
+        {
+            _configuration = configuration;
+            _clientFactory = clientFactory;
+            _logger = loggerFactory.CreateLogger<DownloadFromBrregAndProcess>();
+        }
+
+        [Function("DownloadFromBrregAndProcess")]
+        public async Task RunAsyncTimer([TimerTrigger("0 37 2 1,8,15,22 * *"
+#if DEBUG
+            , RunOnStartup = true
+#endif
+            )] MyInfo myTimer)
+        {
+            _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} timer trigger executed at: {DateTime.Now}");
+            await PerformDownloadAndProcess();
+            _logger.LogInformation($"Next timer schedule for {nameof(DownloadFromBrregAndProcess)} at: {myTimer.ScheduleStatus.Next}");
+        }
+
+        [Function("ForceDownloadFromBrregAndProcess")]
+        public async Task<HttpResponseData> RunAsyncHttp([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+        {
+            _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} manual ER refresh executed at: {DateTime.Now}");
+            
+            await PerformDownloadAndProcess();
+
+            _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} manual ER refresh completed at: {DateTime.Now}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            await response.WriteStringAsync("Done!");
+            
+            return response;
+        }
+
+        private async Task PerformDownloadAndProcess()
+        {
+            var sw = Stopwatch.StartNew();
+
+            var blobServiceClient = new BlobServiceClient(_configuration["AzureWebJobsStorage"]);
+            var containerClient = blobServiceClient.GetBlobContainerClient("minimaler");
+            if (!await containerClient.ExistsAsync())
+            {
+                await containerClient.CreateAsync();
+            }
+            var client = _clientFactory.CreateClient("brreg");
+
+            var response = await client.GetAsync("https://data.brreg.no/enhetsregisteret/oppslag/enheter/lastned",
+                HttpCompletionOption.ResponseHeadersRead);
+
+            _logger.LogInformation("Headers received, starting parse ...");
+
+            var contentStream = await response.Content.ReadAsStreamAsync();
+            var cnt = 0;
+
+            var outputStream = new MemoryStream();
+
+            using (var writer = new StreamWriter(outputStream))
+            using (var gzipStream = new GZipInputStream(contentStream))
+            using (var sr = new StreamReader(gzipStream))
+            using (var reader = new JsonTextReader(sr))
+            {
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonToken.StartObject)
+                    {
+                        var entry = JObject.Load(reader).ToObject<JObject>();
+                        writer.WriteLine($"{entry!["organisasjonsnummer"]!}\t{entry["navn"]!}");
+                    }
+                    cnt++;
+#if DEBUG
+                    if (cnt % 10000 == 0)
+                    {
+                        _logger.LogInformation($"Written {cnt} entries ({(float)cnt/sw.ElapsedMilliseconds*1000} entries/sec)");
+                    }
+#endif
+                }
+
+                _logger.LogInformation("Uploading blob ...");
+                outputStream.Position = 0;
+                var blobClient = containerClient.GetBlobClient("mainunits2.tsv");
+                await blobClient.UploadAsync(outputStream);
+                _logger.LogInformation("Upload complete");
+            }
+
+            var elapsed = sw.ElapsedMilliseconds;
+            var persec = (float)cnt / elapsed * 1000;
+
+            _logger.LogInformation($"Wrote {cnt} entries in {elapsed / 1000} seconds ({persec} entries/sec)");
+        }
+    }
+
+    public class MyInfo
+    {
+        public MyScheduleStatus ScheduleStatus { get; set; } = new();
+
+        public bool IsPastDue { get; set; }
+    }
+
+    public class MyScheduleStatus
+    {
+        public DateTime Last { get; set; }
+
+        public DateTime Next { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+    }
+}
---
+++ minimal-er/Program.cs
@@ -0,0 +1,13 @@
+using System.Net;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services =>
+    {
+        services.AddHttpClient("brreg");
+    })
+    .Build();
+
+host.Run();
