6e9110322b4f97df94ea448edb1bff2c3463a3d4
Bjørn Dybvik Langfors <bdl@digdir.no>
2022-09-29 11:48:41 +0200

    Update dependencies, cleanups

--- minimal-er/DownloadFromBrregAndProcess.cs
+++ minimal-er/DownloadFromBrregAndProcess.cs
@@ -9,119 +9,102 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace minimal_er
+namespace minimal_er;
+
+public class DownloadFromBrregAndProcess
 {
-    public class DownloadFromBrregAndProcess
+    private readonly IConfiguration _configuration;
+    private readonly IHttpClientFactory _clientFactory;
+    private readonly ILogger _logger;
+
+    public DownloadFromBrregAndProcess(ILoggerFactory loggerFactory, IConfiguration configuration, IHttpClientFactory clientFactory)
     {
-        private readonly IConfiguration _configuration;
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly ILogger _logger;
+        _configuration = configuration;
+        _clientFactory = clientFactory;
+        _logger = loggerFactory.CreateLogger<DownloadFromBrregAndProcess>();
+    }
 
-        public DownloadFromBrregAndProcess(ILoggerFactory loggerFactory, IConfiguration configuration, IHttpClientFactory clientFactory)
-        {
-            _configuration = configuration;
-            _clientFactory = clientFactory;
-            _logger = loggerFactory.CreateLogger<DownloadFromBrregAndProcess>();
-        }
-
-        [Function("DownloadFromBrregAndProcess")]
-        public async Task RunAsyncTimer([TimerTrigger("0 37 2 1,8,15,22 * *"
+    [Function("DownloadFromBrregAndProcess")]
+    public async Task RunAsyncTimer([TimerTrigger("0 37 2 1,8,15,22 * *"
 #if DEBUG
-            , RunOnStartup = true
+        , RunOnStartup = true
 #endif
-            )] MyInfo myTimer)
-        {
-            _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} timer trigger executed at: {DateTime.Now}");
-            await PerformDownloadAndProcess();
-            _logger.LogInformation($"Next timer schedule for {nameof(DownloadFromBrregAndProcess)} at: {myTimer.ScheduleStatus.Next}");
-        }
+    )] TimerInfo myTimer)
+    {
+        _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} timer trigger executed at: {DateTime.Now}");
+        await PerformDownloadAndProcess();
+        _logger.LogInformation($"Next timer schedule for {nameof(DownloadFromBrregAndProcess)} at: {myTimer.ScheduleStatus?.Next}");
+    }
 
-        [Function("ForceDownloadFromBrregAndProcess")]
-        public async Task<HttpResponseData> RunAsyncHttp([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
-        {
-            _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} manual ER refresh executed at: {DateTime.Now}");
+    [Function("ForceDownloadFromBrregAndProcess")]
+    public async Task<HttpResponseData> RunAsyncHttp([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+    {
+        _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} manual ER refresh executed at: {DateTime.Now}");
             
-            await PerformDownloadAndProcess();
+        await PerformDownloadAndProcess();
 
-            _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} manual ER refresh completed at: {DateTime.Now}");
+        _logger.LogInformation($"{nameof(DownloadFromBrregAndProcess)} manual ER refresh completed at: {DateTime.Now}");
 
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-            await response.WriteStringAsync("Done!");
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        await response.WriteStringAsync("Done!");
             
-            return response;
-        }
+        return response;
+    }
 
-        private async Task PerformDownloadAndProcess()
+    private async Task PerformDownloadAndProcess()
+    {
+        var sw = Stopwatch.StartNew();
+
+        var blobServiceClient = new BlobServiceClient(_configuration["AzureWebJobsStorage"]);
+        var containerClient = blobServiceClient.GetBlobContainerClient("minimaler");
+        if (!await containerClient.ExistsAsync())
         {
-            var sw = Stopwatch.StartNew();
+            await containerClient.CreateAsync();
+        }
+        var client = _clientFactory.CreateClient("brreg");
 
-            var blobServiceClient = new BlobServiceClient(_configuration["AzureWebJobsStorage"]);
-            var containerClient = blobServiceClient.GetBlobContainerClient("minimaler");
-            if (!await containerClient.ExistsAsync())
+        var response = await client.GetAsync("https://data.brreg.no/enhetsregisteret/oppslag/enheter/lastned",
+            HttpCompletionOption.ResponseHeadersRead);
+
+        _logger.LogInformation("Headers received, starting parse ...");
+
+        var contentStream = await response.Content.ReadAsStreamAsync();
+        var cnt = 0;
+
+        var outputStream = new MemoryStream();
+
+        using (var writer = new StreamWriter(outputStream))
+        using (var gzipStream = new GZipInputStream(contentStream))
+        using (var sr = new StreamReader(gzipStream))
+        using (var reader = new JsonTextReader(sr))
+        {
+            while (reader.Read())
             {
-                await containerClient.CreateAsync();
-            }
-            var client = _clientFactory.CreateClient("brreg");
-
-            var response = await client.GetAsync("https://data.brreg.no/enhetsregisteret/oppslag/enheter/lastned",
-                HttpCompletionOption.ResponseHeadersRead);
-
-            _logger.LogInformation("Headers received, starting parse ...");
-
-            var contentStream = await response.Content.ReadAsStreamAsync();
-            var cnt = 0;
-
-            var outputStream = new MemoryStream();
-
-            using (var writer = new StreamWriter(outputStream))
-            using (var gzipStream = new GZipInputStream(contentStream))
-            using (var sr = new StreamReader(gzipStream))
-            using (var reader = new JsonTextReader(sr))
-            {
-                while (reader.Read())
+                if (reader.TokenType == JsonToken.StartObject)
                 {
-                    if (reader.TokenType == JsonToken.StartObject)
-                    {
-                        var entry = JObject.Load(reader).ToObject<JObject>();
-                        writer.WriteLine($"{entry!["organisasjonsnummer"]!}\t{entry["navn"]!}");
-                    }
-                    cnt++;
-#if DEBUG
-                    if (cnt % 10000 == 0)
-                    {
-                        _logger.LogInformation($"Written {cnt} entries ({(float)cnt/sw.ElapsedMilliseconds*1000} entries/sec)");
-                    }
-#endif
+                    var entry = JObject.Load(reader).ToObject<JObject>();
+                    writer.WriteLine($"{entry!["organisasjonsnummer"]!}\t{entry["navn"]!}");
                 }
-
-                _logger.LogInformation("Uploading blob ...");
-                outputStream.Position = 0;
-                var blobClient = containerClient.GetBlobClient("mainunits2.tsv");
-                await blobClient.UploadAsync(outputStream, overwrite: true);
-                _logger.LogInformation("Upload complete");
+                cnt++;
+#if DEBUG
+                if (cnt % 10000 == 0)
+                {
+                    _logger.LogInformation($"Written {cnt} entries ({(float)cnt/sw.ElapsedMilliseconds*1000} entries/sec)");
+                }
+#endif
             }
 
-            var elapsed = sw.ElapsedMilliseconds;
-            var persec = (float)cnt / elapsed * 1000;
-
-            _logger.LogInformation($"Wrote {cnt} entries in {elapsed / 1000} seconds ({persec} entries/sec)");
+            _logger.LogInformation("Uploading blob ...");
+            outputStream.Position = 0;
+            var blobClient = containerClient.GetBlobClient("mainunits2.tsv");
+            await blobClient.UploadAsync(outputStream, overwrite: true);
+            _logger.LogInformation("Upload complete");
         }
-    }
 
-    public class MyInfo
-    {
-        public MyScheduleStatus ScheduleStatus { get; set; } = new();
+        var elapsed = sw.ElapsedMilliseconds;
+        var persec = (float)cnt / elapsed * 1000;
 
-        public bool IsPastDue { get; set; }
-    }
-
-    public class MyScheduleStatus
-    {
-        public DateTime Last { get; set; }
-
-        public DateTime Next { get; set; }
-
-        public DateTime LastUpdated { get; set; }
+        _logger.LogInformation($"Wrote {cnt} entries in {elapsed / 1000} seconds ({persec} entries/sec)");
     }
 }
